1585b0dbd2ea5a719260610cd99febda225074a7
root <root@pc-8397.pr.local>
2012-11-30 11:55:59 +0200

    00_hello - done

---
+++ 00_hello/hello.rb
@@ -0,0 +1,9 @@
+# hello function returns static text "Hello!"
+def hello
+  "Hello!"
+end
+
+# greet function returns text "Hello, (who)!". "(who)" - is argument
+def greet(who)
+  "Hello, #{who}!"
+end
