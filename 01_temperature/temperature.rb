6ace4d04144af75eeb248c26f86cc23376e6ce78
Viktor Tershivsky <term87@i.ua>
2012-12-10 18:37:51 +0200

    01_temperature - small fix

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,11 +1,16 @@
+#Constants
+CELSIUS_TO_FARENGATE_KOEF = 9.0 / 5.0
+FARENGATE_TO_CELSIUS_KOEF = 1 / CELSIUS_TO_FARENGATE_KOEF
+CELSIUS_AND_FARENGATE_NULL_KOEF = 32.0
+
 #Function converts farengate temperature to celsius
 def ftoc(farengate_temperature)
   #Using integer math
-  (farengate_temperature - 32) * 5 / 9
+  (farengate_temperature - CELSIUS_AND_FARENGATE_NULL_KOEF) * FARENGATE_TO_CELSIUS_KOEF
 end
 
 #Function converts celsius temperature to farengate
 def ctof(celsius_temperature)
   #Using floating point math
-  celsius_temperature * 9.0 / 5.0 + 32.0
+  celsius_temperature * CELSIUS_TO_FARENGATE_KOEF + CELSIUS_AND_FARENGATE_NULL_KOEF
 end
