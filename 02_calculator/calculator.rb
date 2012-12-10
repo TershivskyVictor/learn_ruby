709af11f37fe9ee73535ad771c4d30639c8584e4
Viktor Tershivsky <term87@i.ua>
2012-12-10 18:57:21 +0200

    Calculator - dev commit(not works)

---
+++ 02_calculator/calculator.rb
@@ -0,0 +1,15 @@
+def add(first_arg, second_arg)
+  first_arg + second_arg
+end
+
+def subtract(first_arg, second_arg)
+  first_arg - second_arg
+end
+
+def sum(values_array)
+#  output = 0
+#  sum.each do|value|
+#    output += value
+#  end
+#  output
+end
