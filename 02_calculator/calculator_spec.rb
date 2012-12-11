e7b2fc69b0c5433ee2911899130ee7bc34fee98e
Viktor Tershivsky <term87@i.ua>
2012-12-11 10:15:27 +0200

    02_calcalator - it works

--- 02_calculator/calculator_spec.rb
+++ 02_calculator/calculator_spec.rb
@@ -78,19 +78,18 @@ end
 # write tests and code for the following:
 
 describe "#multiply" do
-
   it "multiplies two numbers" do
-    multiply(2,4).should == 8
+    multiply(2, 4).should == 8
   end
 
   it "multiplies several numbers" do
-    multiply(2,3,4,5).should == 120
+    multiply(2, 3, 4, 5).should == 120
   end
 end
 
 describe "#power" do
   it "raises one number to the power of another number" do
-    power(4,3).should == 64
+    power(4, 3).should == 64
   end
 end
 
