af9ca833ab5b3995ffad73939e07ef566809b637
Viktor Tershivsky <term87@i.ua>
2012-12-27 13:58:04 +0200

    10_timer - done

--- 10_timer/timer.rb
+++ 10_timer/timer.rb
@@ -1,5 +1,4 @@
 class Timer
-  
   def initialize
     @seconds = 0
   end
@@ -7,6 +6,10 @@ class Timer
   attr_accessor :seconds
 
   def time_string
-    (Time.utc(2013,"jan",1,0,0,0) + @seconds).strftime("%H:%M:%S")
+    # 24h - timer
+    #(Time.utc(2013, "jan", 1, 0, 0, 0) + @seconds).strftime("%H:%M:%S")
+
+    # full h-timer
+    "%02d:%02d:%02d" % [@seconds / 3600, @seconds % 3600 / 60, @seconds % 60]
   end
 end
